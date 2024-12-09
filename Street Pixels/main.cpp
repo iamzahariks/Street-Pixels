249a9f560f7e0c2c8577d63ed4d065c04d20d6e3
Zakhar <zaxapeee@gmail.com>
2024-12-10 01:17:02 +0300

    Добавление окна игры

--- Street Pixels/main.cpp
+++ Street Pixels/main.cpp
@@ -2,5 +2,21 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
+	// Создание окна
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Street Pixel");
+
+	// Рендеринг
+	while (window.isOpen()) {
+		sf::Event event;
+
+		while (window.pollEvent(event)) {
+			if (event.type != sf::Event::Closed) { continue; }
+			window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
+
 	return 0;
 }
